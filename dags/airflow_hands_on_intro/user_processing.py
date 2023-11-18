868a8c01fee8195302a36806254ce7a03c2f9048
Aditya Mathur <adityamathur2018@gmail.com>
2023-11-19 01:06:26 +0530

    extract_user - SimpleHttpOperator (method = 'GET')

--- dags/airflow_hands_on_intro/user_processing.py
+++ dags/airflow_hands_on_intro/user_processing.py
@@ -2,7 +2,9 @@ from airflow import DAG
 from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
+import json
 
 
 with DAG(dag_id = "user_processing",start_date = datetime(2023,1,1),schedule_interval = "@daily",catchup = False) as dag:
@@ -23,3 +25,12 @@ with DAG(dag_id = "user_processing",start_date = datetime(2023,1,1),schedule_int
     is_api_available = HttpSensor(task_id = 'is_api_available', 
                                   http_conn_id = 'user_api',
                                   endpoint = 'api/')
+    
+    extract_user = SimpleHttpOperator(
+                    task_id = 'extract_user',
+                    http_conn_id = 'user_api',
+                    endpoint = 'api/',
+                    method = 'GET',
+                    response_filter = lambda response: json.loads(response.text),
+                    log_response = True
+                    )
