b56a82ff60ebbd2947dc23ea2d57deeb42115a8c
Aditya Mathur <adityamathur2018@gmail.com>
2023-11-18 19:24:17 +0530

    fixing typo

--- dags/airflow_hands_on_intro/user_processing.py
+++ dags/airflow_hands_on_intro/user_processing.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 
 
-with DAG(dag_id = "user_processing",start_time = datetime(2023,1,1),schedule_interval = "@daily",catchup = False) as dag:
+with DAG(dag_id = "user_processing",start_date = datetime(2023,1,1),schedule_interval = "@daily",catchup = False) as dag:
     None
