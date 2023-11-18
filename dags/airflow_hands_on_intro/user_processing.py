52aca4932a2a3efac97d21471bee204814678b48
Aditya Mathur <adityamathur2018@gmail.com>
2023-11-19 00:53:42 +0530

    adding HttpSensor

--- dags/airflow_hands_on_intro/user_processing.py
+++ dags/airflow_hands_on_intro/user_processing.py
@@ -1,14 +1,14 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-
+from airflow.providers.http.sensors.http import HttpSensor
 
 
 
 with DAG(dag_id = "user_processing",start_date = datetime(2023,1,1),schedule_interval = "@daily",catchup = False) as dag:
     
-    create_table = PostgresOperator(task_id="create_table",
-                                    postgres_conn_id="postgres",
+    create_table = PostgresOperator(task_id='create_table',
+                                    postgres_conn_id='postgres',
                                     sql = '''
                                             CREATE TABLE IF NOT EXISTS users(
                                                 first_name TEXT NOT NULL,
@@ -19,3 +19,7 @@ with DAG(dag_id = "user_processing",start_date = datetime(2023,1,1),schedule_int
                                                 email TEXT NOT NULL
                                             )
                                             ''')
+    
+    is_api_available = HttpSensor(task_id = 'is_api_available', 
+                                  http_conn_id = 'user_api',
+                                  endpoint = 'api/')
