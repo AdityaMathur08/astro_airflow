b4de49c51c1942d917dca650648a80da83de4ba5
Aditya Mathur <adityamathur2018@gmail.com>
2023-11-19 00:22:10 +0530

    add first task - create_table

--- dags/airflow_hands_on_intro/user_processing.py
+++ dags/airflow_hands_on_intro/user_processing.py
@@ -1,8 +1,21 @@
 from airflow import DAG
 from datetime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
 
 
 with DAG(dag_id = "user_processing",start_date = datetime(2023,1,1),schedule_interval = "@daily",catchup = False) as dag:
-    None
+    
+    create_table = PostgresOperator(task_id="create_table",
+                                    postgres_conn_id="postgres",
+                                    sql = '''
+                                            CREATE TABLE IF NOT EXISTS users(
+                                                first_name TEXT NOT NULL,
+                                                last_name TEXT NOT NULL,
+                                                country TEXT NOT NULL,
+                                                username TEXT NOT NULL,
+                                                password TEXT NOT NULL,
+                                                email TEXT NOT NULL
+                                            )
+                                            ''')
