27337f012a336041a068e2b246579ff9b9087795
Aditya Mathur <adityamathur2018@gmail.com>
2023-11-21 14:38:52 +0530

    fixing dag import error

--- dags/airflow_hands_on_intro/group_dag.py
+++ dags/airflow_hands_on_intro/group_dag.py
@@ -1,9 +1,32 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.subdag import SubDagOperator
-from subdag_downloads import subdag_downloads
+#from subdag_downloads import subdag_downloads
  
 from datetime import datetime
+
+
+def subdag_downloads(parent_dag_id,child_dag_id,args):
+    with DAG(f"{parent_dag_id}.{child_dag_id}",
+             start_date = args['start_date'],
+             schedule_interval = args['schedule_interval'],
+             catchup = args['catchup']):
+        download_a = BashOperator(
+            task_id='download_a',
+            bash_command='sleep 10'
+        )
+ 
+        download_b = BashOperator(
+            task_id='download_b',
+            bash_command='sleep 10'
+        )
+    
+        download_c = BashOperator(
+            task_id='download_c',
+            bash_command='sleep 10'
+        )
+
+        return DAG
  
 with DAG('group_dag', start_date=datetime(2022, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
